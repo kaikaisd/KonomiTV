dc178822143cec796dfd525186ba8990a30fc576
huggy <i@huggy.moe>
2025-02-06 23:44:03 +0900

    Refactor: [Client][Channel/Logo] チャンネルロゴを一つずつリクエストする代わりにスプライト（タイル）ロゴを使用する

--- client/src/components/Watch/Panel/RecordedProgram.vue
+++ client/src/components/Watch/Panel/RecordedProgram.vue
@@ -5,8 +5,12 @@
                 v-html="ProgramUtils.decorateProgramInfo(playerStore.recorded_program, 'title')">
             </h1>
             <div class="program-info__broadcaster">
-                <img class="program-info__broadcaster-icon"
-                    :src="`${Utils.api_base_url}/channels/${playerStore.recorded_program.channel?.id ?? 'NID0-SID0'}/logo`">
+                <div class="program-info__broadcaster-icon">
+                    <div class="ch-sprite" :chid="playerStore.recorded_program.channel?.id ?? 'NID0-SID0'">
+                        <img loading="lazy"
+                            :src="`${Utils.api_base_url}/channels/${playerStore.recorded_program.channel?.id ?? 'NID0-SID0'}/logo`">
+                    </div>
+                </div>
                 <div class="program-info__broadcaster-container">
                     <div class="d-flex align-center" v-if="playerStore.recorded_program.channel !== null">
                         <div class="program-info__broadcaster-number">Ch: {{playerStore.recorded_program.channel.channel_number}}</div>
@@ -263,9 +267,12 @@ export default defineComponent({
             &-icon {
                 display: inline-block;
                 flex-shrink: 0;
-                width: 44px;
-                height: 36px;
-                border-radius: 3px;
+                --ch-sprite-width: 44;
+                --ch-sprite-height: 36;
+                --ch-sprite-border-radius: 3;
+                width: calc(var(--ch-sprite-width) * 1px);
+                height: calc(var(--ch-sprite-height) * 1px);
+                border-radius: calc(var(--ch-sprite-border-radius) * 1px);
                 background: linear-gradient(150deg, rgb(var(--v-theme-gray)), rgb(var(--v-theme-background-lighten-2)));
                 object-fit: cover;
                 user-select: none;
